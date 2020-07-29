790781e514996a851d41ea3802c8da635b3edcc4
Kasun Nirmala <bgkasunnirmala@gmail.com>
2020-07-29 12:21:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { BaseChartDirective, Color, Label } from 'ng2-charts';
+import { ChartDataSets, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'smartbin-ng';
+export class AppComponent implements OnInit {
+
+  data = [100];
+  public lineChartLabels: Label[] = ['test'];
+  constructor(private socket: Socket) {
+
+    this.socket.on('new-message', (message) => {
+      // alert(message);
+      this.data.push(parseFloat(message));
+      this.lineChartLabels.push(new Date().toISOString());
+    });
+  }
+  public lineChartData: ChartDataSets[] = [
+    { data: this.data, label: 'HEIGHT' },
+  ];
+
+  public lineChartType = 'line';
+
+  ngOnInit() {
+  }
+
+
 }
